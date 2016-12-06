bbc3e50f0c146c6ea6d79194570ac6a181ec2550
Jasper Swaagman <jasper.swaagman@belsimpel.nl>
2016-12-06 13:29:07 +0100

    day 4 complete

--- day4/part2.py
+++ day4/part2.py
@@ -2,16 +2,23 @@ import re
 from collections import Counter
 from operator import itemgetter
 
-with open("smallinput", "r") as fp:
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
+    'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def decoder(char, shift):
+    # Make a loop trough the alphabet
+    return alphabet[(alphabet.index(char) + shift) % 26]
+
+
+with open('input', 'r') as fp:
     for line in fp:
         line = line.strip()
         
         # get the needed info
-        checksum = re.search("\[(\w{5})\]", line)
-        checksum = checksum.group(1)
-        r = re.search("([a-z-]+)(\d+)", line)
-        enc_name = r.group(1).replace("-","")
+        r = re.search('([a-z-]+)(\d+)\[(\w{5})\]', line)
+        enc_name = r.group(1).replace('-','')
         sector_id = int(r.group(2))
+        checksum = r.group(3)
 
         letters = []
         for c in enc_name: letters.append(c)
@@ -21,8 +28,9 @@ with open("smallinput", "r") as fp:
         letters.sort(key=itemgetter(1), reverse=True)
         most_common = [] 
         for c in range(5): most_common.append(letters[c][0])
-        if "".join(most_common) == checksum: 
-            print(r.group(1))
-            # Loop through the string and shift every char sector_id times
-            # if string == North Pole: print(sector_id)
+        # If room is valid 
+        if ''.join(most_common) == checksum: 
+            decoced_line = ''
+            for c in enc_name: decoced_line = decoced_line + decoder(c, sector_id)
+            if 'northpole' in decoced_line: print(sector_id); break
 
