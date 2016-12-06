5d40925a460a010663bfedd99f6d514c9b8a2de9
Jasper Swaagman <jasper.swaagman@belsimpel.nl>
2016-12-06 17:44:57 +0100

    day 5 start

---
+++ day5/party1.py
@@ -0,0 +1,20 @@
+import re
+from collections import Counter
+from operator import itemgetter
+
+password = []
+
+def decoder(char, shift):
+    # Make a loop trough the alphabet
+    return alphabet[(alphabet.index(char) + shift) % 26]
+
+
+i = 0
+input = # need content of file here
+for line in fp:
+    line = line.strip()
+    #while not hash_found: 
+    while i < 5: 
+        line = + str(i)
+        i += 1
+    print(line)
