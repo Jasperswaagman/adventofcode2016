04935b198a5eb4f3dafbf3495e1b379443e09b1a
Jasper Swaagman <jasperswaagman@gmail.com>
2016-12-07 01:46:45 +0100

    start of day6

---
+++ day6/part1.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from operator import itemgetter
+
+i = 0
+message = [[] for _ in range(6)]
+message_final = ''
+
+with open('input') as f: 
+    for line in f:
+        line = line.strip()
+        for i in range(6):
+            message[i].append(line[i])
+
+for x in range(len(message)):
+    c = Counter(message[x]).most_common()
+    print(c)
+    c.sort(key=itemgetter(1), reverse=True)
+    message_final = message_final + c[0][0]
+
+print(message_final)
