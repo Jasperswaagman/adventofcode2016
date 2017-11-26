b6e694f2f9ffb1f801b27c7b9eff7f14693dfaa3
Jasper Swaagman <jasperswaagman@gmail.com>
2017-11-27 00:22:49 +0100

    aoc 2017 warmup

--- day8/part1.py
+++ day8/part1.py
@@ -1,53 +1,63 @@
-import re, time, sys
+# vim: et sw=4 ts=4 autoindent
+import re, copy
+from collections import Counter
 
-screen_width = 50
-screen_height = 6
+class Display:
+    def __init__(self, width, height):
+        self.grid = [['.' for x in range(width)] for y in range(height)]
+    
+    def render(self):
+        print("\033c")
+        for row in self.grid:
+            print(''.join([str(x) for x in row]))
 
-def init_input():
-    with open('small_input') as f:
-        o = f.readlines()
-        o = [x.strip() for x in o]
-        return o
+    def shift_col(self, x):
+        tmp = self.grid[-1][x]
+        copyGrid = copy.deepcopy(self.grid)
+        for i in range(len(self.grid) - 1):
+            self.grid[1+i][x] = copyGrid[i][x]
+        self.grid[0][x] = tmp
+    
+    def shift_row(self, x):
+        tmp = self.grid[x][-1]
+        copyGrid = copy.deepcopy(self.grid)
+        for i in range(len(self.grid[0]) - 1):
+            self.grid[x][1+i] = copyGrid[x][i]
+        self.grid[x][0] = tmp
 
-def init_display():
-    global screen_width, screen_height
-    s_x = []
-    for _ in range(screen_width): s_x.append('.')
-    screen = {}
-    for i in range(screen_height): screen[i] = s_x
-    return screen
+    def rect(self, width, height):
+        for row in range(height):
+            for x in range(width):
+               self.grid[row][x] = '#' 
+        self.render()
 
-def print_screen(s):
-    sys.stdout.write('\033c')
-    for c in s: print(''.join(s[c]))
+    def rotate(self, t, x, amount):
+        if t == 'column':
+            for i in range(amount):
+                self.shift_col(x)
+        else:
+            for i in range(amount):
+                self.shift_row(x)
+        self.render()
 
-def rect(o):
-    global screen
-    coords = re.search('(\d+)x(\d+)', o)
-    x = int(coords.group(1))
-    y = int(coords.group(2))
-    # draw a x by y rectangle in our screen
-    for i in range(y):
-        new_row = []
-        for _ in range(x):
-            new_row.append('#')
-        for _ in range(x, screen_width):
-            new_row.append('.')
-        screen[i] = new_row
 
-def rotate_row(o):
-    pass
+with open('input') as f:
+    instructions = f.readlines()
+    instructions = [x.strip() for x in instructions]
 
-def rotate_col(o):
-    pass
+display = Display(50,6)
 
-operations = init_input()
-screen = init_display()
+for i in instructions:
+    wordList = re.sub('[^\w]', ' ', i).split()
+    if wordList[0] == 'rect': 
+        coordinates = re.findall('\d+', wordList[1])
+        display.rect(int(coordinates[0]), int(coordinates[1]))
+    elif wordList[0] == 'rotate':
+        display.rotate(wordList[1], int(wordList[3]), int(wordList[5]))
+    else:
+        print('Bad instruction: {0}'.format(i))
+        raise SystemExit(1)
 
-for o in operations:
-    if o.startswith('rect'): rect(o);
-    elif o.startswith('rotate r'): rotate_row(o)
-    else: rotate_col(o)
-    time.sleep(.2)
-    print_screen(screen)
+c = Counter([i for s in display.grid for i in s])
+print(c['#'])
 
